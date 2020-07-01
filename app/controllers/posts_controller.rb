0b90033c1d5015c51eb328ef560066ea0ec26535
yuta <fantasista098@gmail.com>
2020-07-01 17:21:36 +0900

    コントローラーでflashメッセージを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create!(post_params)
-    redirect_to post
+    redirect_to post, notice: "投稿しました"
   end
 
   def edit
@@ -22,12 +22,12 @@ class PostsController < ApplicationController
 
   def update
     @post.update!(post_params)
-    redirect_to post
+    redirect_to post, notice: "更新しました"
   end
 
   def destroy
     @post.destroy!
-    redirect_to post
+    redirect_to post, notice: "削除しました"
   end
 
   def set_post
